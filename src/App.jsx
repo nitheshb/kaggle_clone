05134eee09123b4f89bfc06c9ef5b2c24e25595d
Divya p <divyasri.ponnaganti@ensarsolutions.com>
2025-05-12 19:46:42 +0530

    competition

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import Allcomponents from './components/All competitions/Allcomponents';
+import FeaturedPage from './components/Featured/Featured';
+
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600">
-        Tailwind is Active!
-      </h1>
+   <div className="min-h-screen bg-gray-100 px-6 py-8">
+
+           <Allcomponents />
+          {/* <FeaturedPage /> */}
     </div>
   );
 }
